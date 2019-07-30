a64a101de4eb114ac63d14747731fa649efa7e21
iD Crypt3 <idcrypt3@gmail.com>
2019-07-30 16:56:46 -0700

    Tuesday Ashley commit

--- AshleyK/RewritingTheAlphabet.py
+++ AshleyK/RewritingTheAlphabet.py
@@ -2,10 +2,8 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 partialOne = ""
 partialTwo = ""
 newAlphabet = ""
-message = input("Please enter a secret message: ")
-message = input(" ... ").lower()
-key = input("Please enter a number to shift by: ")
-key = int(input(" ... "))
+message = input("Please enter a secret message: ").lower()
+key = int(input("Please enter a number to shift by: "))
 if key == 0:
     newAlphabet = alphabet
 elif key > 0:
@@ -16,3 +14,9 @@ else:
     partialOne = alphabet[:26 + key]
     partialTwo = alphabet[(26 + key):]
     newAlphabet = partialTwo + partialOne
+for i in range(len(message[i]))
+    index = alphabet.find(message[i])
+    if index < 0:
+        newMessage += message[i]
+    else:
+        newMessage += newAlphabet[index]
