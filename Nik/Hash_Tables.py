63f9371b5e393076f92a84bf801975dd9d44141d
idCrypt3 <idCrypt3@gmail.com>
2019-07-30 14:03:54 -0700

    Hashed book

--- Nik/Hash_Tables.py
+++ Nik/Hash_Tables.py
@@ -7,6 +7,15 @@ phone_book = [
  ("Eve", 7642398541),
  ("Charlie", 5052358753)
  ]
+def new_hash(message):
+
+    digest = 0
+    if message == "":
+        digest = -1
+    for c in message:
+        digest += ord(c)
+    digest = digest % max_size
+    return digest
 max_size = 19
 while len(phone_book) < max_size - 1:
  phone_book.append(("", 0000000000))
@@ -27,10 +36,10 @@ for i in range(len(phone_book)):
     if search_count == max_size:
         print("name not found")
 
-search_index = len(sorted_book) // 2
- search_max = len(sorted_book) - 1
- search_min = 0
- search_count = 0
+search_index = int(len(sorted_book) / 2)
+search_max = len(sorted_book) - 1
+search_min = 0
+search_count = 0
 for i in range(len(sorted_book)):
     search_count += 1
     if search_name == sorted_book[search_index][0]:
@@ -45,3 +54,28 @@ for i in range(len(sorted_book)):
         search_index += int((search_max - search_index) / 2 + 0.5)
 if search_count == len(sorted_book):
      print("Name not found")
+phone_hash = []
+hashed_book = [("", 0000000000) for _ in range(max_size)]
+for i in range(len(phone_book)):
+    name = phone_book[i][0]
+    digest = new_hash(name)
+    if digest == -1:
+        continue
+    phone_hash.append(digest)
+    new_index = digest
+    while hashed_book[new_index] != ("", 0000000000):
+        new_index = (new_index + 1) % max_size
+    hashed_book[new_index] = phone_book[i]
+
+hash_index = new_hash(search_name)
+search_count = 0
+for i in range(len(hashed_book)):
+    search_count += 1
+    if hashed_book[hash_index][0] == search_name:
+        print("Hashed book search:")
+        print(hashed_book[hash_index][1])
+        print(search_count)
+        break
+    hash_index = (hash_index + 1) % max_size
+    if search_count == max_size:
+        print("Not found (hashed)")
