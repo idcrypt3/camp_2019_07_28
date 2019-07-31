f96d88ddb86221545389d4ba351a64388a6502b9
iDCrypt3 <iDCrypt3@gmail.com>
2019-07-31 16:44:53 -0700

    Bert crypto

--- Bert/code.py
+++ Bert/code.py
@@ -1,7 +1,7 @@
 def shift_cipher(message, key):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     partialOne = ""
-    partialTwo =  ""
+    partialTwo = ""
     newAlphabet = ""
 
     if key == 0:
@@ -9,37 +9,27 @@ def shift_cipher(message, key):
     elif key > 0:
         partialOne = alphabet[:key]
         partialTwo = alphabet[key:]
+        newAlphabet = partialTwo + partialOne
     else:
         partialOne = alphabet[:(26 + key)]
-        partialTwo = alphabet[(26 + key)]
+        partialTwo = alphabet[(26 + key):]
         newAlphabet = partialTwo + partialOne
+
+    newMessage = ""
     for i in range(0, len(message)):
         index = alphabet.find(message[i])
-    newMessage = ""
-    if index < 0:
-        newMessage += message[i]
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        attempt = ""
-        message = "wpau iwt ephhldgs udg iwt uxghi rajt xh tctgvxots"
-
-
-        def decode(message):
-            for key in range(len(alphabet)):
-                newAlphabet = alphabet[key:] + alphabet[:key]
-                attempt = ""
-
-
-        for i in range(len(message)):
-            index = alphabet.find(message[i])
+        if index < 0:
+            newMessage += message[i]
         else:
-            attempt += newAlphabet[index]
-    print("Key:" + str(key) + "-" + attempt)
-    decode(message)
+            newMessage += newAlphabet[index]
+    return newMessage
+
 
 def main():
     message = input("Please enter a secret message:")
     key = int(input("pass"))
     encrypted = shift_cipher(message, key)
     print(encrypted)
+
 if __name__ == "__main__":
     main()
