1317090d888aaf1fd5644d65571c42c84fce04a9
iDCrypt3 <iDCrypt3@gmail.com>
2019-07-31 09:31:58 -0700

    Griffin add "MAC"

---
+++ Griffin/MAC.py
@@ -0,0 +1,33 @@
+def sign_message(message, key):
+    message_int = 0
+    for c in message:
+        message_int += ord(c)
+    a = key[0]
+    b = key[1]
+    mac_tag = (a * message_int + b) % p
+    return mac_tag
+
+
+p = 491
+message = "Hello World"
+key = [15, 20]
+mac = sign_message(message, key)
+
+
+def check_mac(old_mac, new_mac):
+    if old_mac == new_mac:
+        print("Message is Valid!")
+    else:
+        print("Message is Compromised!")
+
+
+message1 = "Hello world"
+message2 = "Hello World"
+
+
+mac1 = sign_message(message1, key)
+mac2 = sign_message(message2, key)
+
+
+check_mac(mac, mac1)
+check_mac(mac, mac2)
