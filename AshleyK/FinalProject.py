9d39097daccd06c2c960b49e7c561d5dc539942c
iD Crypt3 <idcrypt3@gmail.com>
2019-08-01 16:03:18 -0700

    Thu Ashley commit

--- AshleyK/FinalProject.py
+++ AshleyK/FinalProject.py
@@ -1,8 +1,5 @@
 import os, io
 
-# uncomment the 3 lines below and replace the names of your files (do not include .py) and function defs
-# leave "as name" as-is; this renames your functions so they are all compatible with this program,
-# regardless of what you named them
 from RewritingTheAlphabet import shift_cipher as shift_cypher
 from BlockCipher import pad_message as block_pad, rebuild_message as block_rebuild
 from BlockCipher import apply_shift as block_shift, undo_shift as block_unshift
@@ -10,9 +7,9 @@ from DiffieHellman import find_shared_key as dh_shared_key, apply_shift as dh_sh
 
 dh_base = 8
 dh_mod = 29
-dh_private_key = 49
+dh_private_key = 219
 dh_public_key = dh_base ** dh_private_key % dh_mod
-
+print(dh_public_key)
 
 def main():
     print("Hello iD Campers, Parents, and Staff!")
@@ -70,8 +67,7 @@ def encrypt():
             encrypted = "\n".join(str(s) for s in encrypted)
             break
         elif cypher == 3:
-            msg_public_key = dh_base ** data[1] % dh_mod
-            shared_key = dh_shared_key(dh_private_key, msg_public_key)
+            shared_key = dh_shared_key(dh_private_key, data[1])
             encrypted = dh_shift(data[0], shared_key)
             break
         elif cypher == 0:
@@ -168,22 +164,3 @@ def get_key():
 
 if __name__ == "__main__":
     main()
-
-# Ideas for new features:
-# - Include your name or contact info in the comments and/or opening scroll.
-# - Write some messages or stories and encrypt and save them to disk for your family and friends to discover.
-# - Include color codes - red for failed encryption, green for passed (see the lesson Hexadecimal\Character Codes).
-# - This program includes functionality you haven't seen in the form of file I/O, string formatting, and imported
-# modules. See if you understand what's going on and reference the online documentation if you don't.
-# - Errors are handled, but the user navigation could be more friendly (e.g. allowing users to return to a previous menu
-# rather than forcing them to stick with the choice to encrypt or decrypt, even if they change their mind). Try expand-
-# ing it!
-# Prevent the user from attempting a Ceaser shift greater than +-26, or use mod (%) to correct it
-
-# Advanced features:
-# - Create a puzzle for users to solve by slowly ramping up the difficulty (e.g., the key to a block cypher could be
-# written in a ceaser cypher (as a word - remember, our ceaser cypher only substitutes letters), and that block cypher
-# could have a clue to a Diffie-Hellman cypher, and...)
-# - Display the checksum or hash of messages as they are encrypted and decrypted.
-# You could even save the checksum/hash alongside the messages, so users know if a file has been modified.
-# - Expand your cyphers with more options, or write a new one from internet tutorials.
